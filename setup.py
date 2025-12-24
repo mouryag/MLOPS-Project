d999f6b9badbb43dcc55cd55e7e478ebd912ff85
mouryag <mouryag99@06aug>
2025-12-24 22:45:48 +0530

    small code changes

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup, find_packages
 from typing import List
-from test import get_requirements
+# from test import get_requirements
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()    
-   
+
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 __version__ = "0.0.4"
 REPO_NAME = "MLOPS-Project"
